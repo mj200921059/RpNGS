e1b36f408b3120495ec1a33a9e0f2347ba1a67cc
nwu_majun <mj200921059@hotmail.com>
2025-05-09 08:59:31 +0800

    Update summary_module.R

--- modules/summary_module.R
+++ modules/summary_module.R
@@ -1,25 +1,78 @@
 # Summary Dataset 模块
 
 # UI 部分
+# mod_summary_ui <- function(id) {
+#   ns <- NS(id)  # 用于命名空间
+#   tabPanel(h4('Summary Dataset'), value = 'startsets',
+#            includeMarkdown(paste0(getwd(),"/www/title.Rmd") ),
+#            div(style = "height:2px;border-width:0;color:gray;background-color:gray", hr()),
+#            # Single Animated Chart
+#            column(6, plotlyOutput(ns("location_stat"), width = "auto", height = "300px") %>% withSpinner(type = 5)),
+#            column(3, plotlyOutput(ns("sampletype_stat"), width = "auto", height = "300px") %>% withSpinner(type = 5)),
+#            column(3, plotlyOutput(ns("month_stat"), width = "auto", height = "300px") %>% withSpinner(type = 5)),
+#            column(12, div(style = "height:2px;border-width:0;color:gray;background-color:gray", hr()),
+#                   div(style = "color:#337ab7;text-align:left;", titlePanel("All Sequenced samples:"))
+#            ),
+#            DT::dataTableOutput(ns('datatable')),
+#            br(),
+#            #includeHTML("./www/html/footer_v4.html")
+#            includeHTML(paste0(getwd(),"/www/html/footer_v4.html"))
+#   )
+# }
 mod_summary_ui <- function(id) {
   ns <- NS(id)  # 用于命名空间
   tabPanel(h4('Summary Dataset'), value = 'startsets',
-           includeMarkdown(paste0(getwd(),"/www/title.Rmd") ),
+           includeMarkdown(paste0(getwd(),"/www/title.Rmd")),
            div(style = "height:2px;border-width:0;color:gray;background-color:gray", hr()),
-           # Single Animated Chart
-           column(6, plotlyOutput(ns("location_stat"), width = "auto", height = "300px") %>% withSpinner(type = 5)),
-           column(3, plotlyOutput(ns("sampletype_stat"), width = "auto", height = "300px") %>% withSpinner(type = 5)),
-           column(3, plotlyOutput(ns("month_stat"), width = "auto", height = "300px") %>% withSpinner(type = 5)),
-           column(12, div(style = "height:2px;border-width:0;color:gray;background-color:gray", hr()),
+           
+           # Original summary visualizations (top row)
+           fluidRow(
+             column(6, plotlyOutput(ns("location_stat"), width = "auto", height = "300px") %>% withSpinner(type = 5)),
+             column(3, plotlyOutput(ns("sampletype_stat"), width = "auto", height = "300px") %>% withSpinner(type = 5)),
+             column(3, plotlyOutput(ns("month_stat"), width = "auto", height = "300px") %>% withSpinner(type = 5))
+           ),
+           
+           # New pathogen analysis section
+           column(12, 
+                  div(style = "height:2px;border-width:0;color:gray;background-color:gray", hr()),
+                  div(style = "color:#337ab7;text-align:left;", h3("Pathogen Analysis"))
+           ),
+           
+           # Pathogen analysis controls and visualization
+           fluidRow(
+             column(3,
+                    wellPanel(
+                      selectInput(ns("infectionType"), "Select Infection Type:",
+                                  choices = c("respiratory tract infections", "CNS infections", "bloodstream infections"),
+                                  multiple = TRUE,
+                                  selected = "respiratory tract infections"),
+                      sliderInput(ns("minSamples"), "Minimum Sample Count:",
+                                  min = 1, max = 20, value = 1),
+                      radioButtons(ns("plotType"), "Plot Type:",
+                                   choices = c("Bar Plot" = "bar", 
+                                               "Heatmap" = "heatmap",
+                                               "Bubble Plot" = "bubble")),
+                      downloadButton(ns("downloadData"), "Download Data")
+                    )
+             ),
+             column(9,
+                    tabsetPanel(
+                      tabPanel("Pathogen Visualization", plotlyOutput(ns("pathogenPlot")) %>% withSpinner(type = 5)),
+                      tabPanel("Pathogen Data", DTOutput(ns("pathogenTable")) %>% withSpinner(type = 5))
+                    )
+             )
+           ),
+           
+           # Original bottom section
+           column(12, 
+                  div(style = "height:2px;border-width:0;color:gray;background-color:gray", hr()),
                   div(style = "color:#337ab7;text-align:left;", titlePanel("All Sequenced samples:"))
            ),
-           DT::dataTableOutput(ns('datatable')),
+           DT::dataTableOutput(ns('datatable')) %>% withSpinner(type = 5),
            br(),
-           #includeHTML("./www/html/footer_v4.html")
            includeHTML(paste0(getwd(),"/www/html/footer_v4.html"))
   )
 }
-
 # Server 逻辑
 mod_summary_server <- function(id, datafr2, summary_sub_datafr2) {
   moduleServer(id, function(input, output, session) {
@@ -78,6 +131,72 @@ mod_summary_server <- function(id, datafr2, summary_sub_datafr2) {
       }
       
     })
+    
+    # Filter data based on user inputs
+    filtered_data <- reactive({
+      pathogen_summary %>%
+        filter(Infections %in% input$infectionType,
+               sample_count >= input$minSamples)
+    })
+    
+    # Create plot based on selected type
+    output$pathogenPlot <- renderPlotly({
+      data <- filtered_data()
+      
+      if (input$plotType == "bar") {
+        p <- ggplot(data, aes(x = reorder(pathogen_name, sample_count), 
+                              y = sample_count, 
+                              fill = Infections)) +
+          geom_bar(stat = "identity") +
+          coord_flip() +
+          labs(x = "Pathogen", y = "Number of Samples", 
+               title = "Pathogen Detection Frequency") +
+          theme_minimal() +
+          theme(legend.position = "bottom")
+        
+      } else if (input$plotType == "heatmap") {
+        p <- ggplot(data, aes(x = Infections, y = pathogen_name, 
+                              fill = sample_count)) +
+          geom_tile() +
+          scale_fill_gradient(low = "white", high = "steelblue") +
+          labs(x = "Infection Type", y = "Pathogen", 
+               title = "Sample Count Heatmap") +
+          theme_minimal() +
+          theme(axis.text.x = element_text(angle = 45, hjust = 1))
+        
+      } else if (input$plotType == "bubble") {
+        p <- ggplot(data, aes(x = Infections, y = pathogen_name, 
+                              size = sample_count, 
+                              color = total_reads)) +
+          geom_point(alpha = 0.7) +
+          scale_size(range = c(3, 15)) +
+          scale_color_gradient(low = "blue", high = "red") +
+          labs(x = "Infection Type", y = "Pathogen", 
+               size = "Sample Count", color = "Total Reads",
+               title = "Bubble Plot of Pathogen Distribution") +
+          theme_minimal() +
+          theme(axis.text.x = element_text(angle = 45, hjust = 1))
+      }
+      
+      ggplotly(p) %>% layout(autosize = TRUE)
+    })
+    
+    # Display data table
+    output$pathogenTable <- DT::renderDT({
+      datatable(filtered_data(), 
+                options = list(pageLength = 10, scrollX = TRUE),
+                rownames = FALSE)
+    })
+    
+    # Download handler
+    output$downloadData <- downloadHandler(
+      filename = function() {
+        paste("pathogen-summary-", Sys.Date(), ".csv", sep="")
+      },
+      content = function(file) {
+        write.csv(filtered_data(), file, row.names = FALSE)
+      }
+    )
     
     
   })
